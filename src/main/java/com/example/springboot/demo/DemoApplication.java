7fc903a0d582e6d187f847ed07ef4d93d189b1ef
Joel Crisanto <joel.crisanto@ibm.com>
2019-04-01 12:19:53 -0600

    First commit for this project; https://www.baeldung.com/spring-boot-start at step 5

---
+++ src/main/java/com/example/springboot/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+/*
+*https://www.baeldung.com/spring-boot-start
+ */
+
+package com.example.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springboot/demo/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.springboot.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+//we’re allowing unrestricted access to all endpoints.
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and().csrf().disable();
+    }
+}
